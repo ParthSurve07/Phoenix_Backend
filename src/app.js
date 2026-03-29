5209b0220ca2cfafee059fd16ca5787bd8d3bf6f
Parth Surve <surveparth429@gmail.com>
2026-03-29 15:16:54 +0530

    fix: added vercel link to cors origin

--- src/app.js
+++ src/app.js
@@ -12,7 +12,8 @@ import userRoutes from "./modules/users/users.routes.js";
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({
+  origin: "http://localhost:3000" || "https://phoenix-frontend-e9qz.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
